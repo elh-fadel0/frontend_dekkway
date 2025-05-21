2d6df77875cafcdc8aad6161d2e078a171f4e236
El hadji <elhadjifallou070@gmail.com>
2025-05-21 03:50:26 +0000

    deploiment

--- frontend_dekkway/frontend/next.config.ts
+++ frontend_dekkway/frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['127.0.0.1', 'localhost', 'api.dekkway.com'], // Ajoutez votre domaine de production ici
+    domains: ['vercel.app'], // Ajoutez votre domaine de production ici
   },
   env: {
     API_URL: process.env.NEXT_PUBLIC_API_URL,
